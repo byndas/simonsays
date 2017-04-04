f6a54b273ceb1a8324ebbb9f62075f4b3724e0bf
marc byndas <byndasma@gmail.com>
2017-04-04 14:47:34 -0400

    test

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use('css', express.static(process.cwd() + '/css'));
-app.use('js', express.static(process.cwd() + '/js'));
+app.use('/css', express.static(process.cwd() + '/css'));
+app.use('/js', express.static(process.cwd() + '/js'));
 app.use(favicon(path.join(__dirname,'favicon','favicon.ico')));
 
 app.get('/', function(req, res){
-			res.sendFile(process.cwd() + '/index.html');
+			res.sendFile(process.cwd() + '/main.html');
 		});
 
 const port = process.env.PORT;
