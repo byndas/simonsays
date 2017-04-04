2808dfaaa1cb42ab3c964b9f6872a0e9743b20eb
marc byndas <byndasma@gmail.com>
2017-04-04 14:38:56 -0400

    test

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use('/css', express.static(process.cwd() + '/css'));
-app.use('/js', express.static(process.cwd() + '/js'));
+app.use('css', express.static(process.cwd() + '/css'));
+app.use('js', express.static(process.cwd() + '/js'));
 app.use(favicon(path.join(__dirname,'favicon','favicon.ico')));
 
 app.get('/', function(req, res){
